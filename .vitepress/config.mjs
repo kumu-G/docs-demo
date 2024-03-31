1e484116f6ef91c13689e7342c2e44a79c645ffb
jcy <1710684003@qq.com>
2024-03-31 22:39:34 +0800

    搜索框

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -28,5 +28,26 @@ export default defineConfig({
     footer:{
       copyright: "Copyright © 2022-present Evan You"
     },
-  }
+  },
+
+  // 设置搜索框的样式
+  search: {
+    provider: "local",
+    options: {
+      translations: {
+        button: {
+          buttonText: "搜索文档",
+          buttonAriaLabel: "搜索文档",
+        },
+        modal: {
+          noResultsText: "无法找到相关结果",
+          resetButtonTitle: "清除查询条件",
+          footer: {
+            selectText: "选择",
+            navigateText: "切换",
+          },
+        },
+      },
+    },
+  },
 })
